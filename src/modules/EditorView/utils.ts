a71645907148c0111316f706f1351edf54e36702
Michael Webb <michael.webb@neo4j.com>
2025-02-18 14:52:22 +0100

    fix: remove unused caught errors

--- src/modules/EditorView/utils.ts
+++ src/modules/EditorView/utils.ts
@@ -80,7 +80,7 @@ export const safeParse = (str: string | null | undefined, fallback: Record<strin
     if (!str) return fallback;
     try {
         return JSON.parse(str);
-    } catch (e) {
+    } catch {
         return fallback;
     }
 };
@@ -96,7 +96,7 @@ export const calculateQueryComplexity = (schema: GraphQLSchema, query: string, v
 
         console.log("Query complexity: ", complexity);
         return complexity;
-    } catch (error) {
+    } catch {
         console.log("Query complexity calculation failed");
         return -1;
     }
